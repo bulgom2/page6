89d6536f659f24cdfa302abc986bcbd17390a36f
hdj185 <hdj185583@gmail.com>
2023-04-13 17:41:29 +0900

    글 작성 제외 권한 부여

---
+++ src/main/java/com/page6/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package com.page6.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint{
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        if("XMLHttpRequest".equals(request.getHeader("x-requested-width"))) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        } else {
+            response.sendRedirect("/members/login");
+        }
+    }
+}
