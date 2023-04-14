c4106bab2eb00dbcae93495ee7963eb2176ac1a5
정병산불곰 <myvj123@naver.com>
2023-04-14 23:20:04 +0900

    작성폼 수정

--- src/main/java/com/page6/Page6Application.java
+++ src/main/java/com/page6/Page6Application.java
@@ -3,6 +3,9 @@ package com.page6;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Page6Application {
 
@@ -10,4 +13,8 @@ public class Page6Application {
 		SpringApplication.run(Page6Application.class, args);
 	}
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 }
