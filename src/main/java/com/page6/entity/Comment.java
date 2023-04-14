cf6f385cb505a15b36b784e5adbf52a0db407e19
정병산불곰 <myvj123@naver.com>
2023-04-14 20:21:02 +0900

    Update Comment.java

--- src/main/java/com/page6/entity/Comment.java
+++ src/main/java/com/page6/entity/Comment.java
@@ -1,18 +1,18 @@
 package com.page6.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 @Table(name = "comment_tb")
 @Entity
 public class Comment {
@@ -23,20 +23,33 @@ public class Comment {
     private Long id;        // 댓글 인덱스
 
     @ManyToOne
-    @JoinColumn(name = "BOARD_FK")
+    @JoinColumn(name = "BOARD_FK", nullable = false)
     private Board board;    // 작성 게시글
 
     @ManyToOne
-    @JoinColumn(name = "MEMBER_FK")
-    private Member member; // 작성자
+    @JoinColumn(name = "MEMBER_FK", nullable = false)
+    private Member writer; // 작성자
 
     @Column(name="COMMENT_CONTENT", columnDefinition = "TEXT", nullable = false)
-    private String comment; // 댓글 내용
-
-    @Column(name="COMMENT_GROUP")
-    private int group; // 댓글 계층 (null이면 그냥 댓글. 부모 댓글 인덱스가 있다면 대댓글)
+    private String content; // 댓글 내용
 
     @CreationTimestamp
     @Column(name="COMMENT_REGDT", updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime regdate;  //등록일
+
+    @Column(name="COMMENT_DEPTH")
+    private int depth;  //깊이 (0이면 일반 댓, 그 이상이면 대댓)
+
+    @Column(name="COMMENT_GROUP")
+    private long group;  //댓글 그룹
+
+
+//    @ManyToOne
+//    @JoinColumn(name = "COMMENT_PARENT")
+//    private Comment parent; // 댓글 계층 (null이면 그냥 댓글. 부모 댓글 인덱스가 있다면 대댓글)
+//
+//    @Builder.Default
+//    @OneToMany(mappedBy = "parent", orphanRemoval = true)
+//    private List<Comment> children = new ArrayList<>();
+
 }
