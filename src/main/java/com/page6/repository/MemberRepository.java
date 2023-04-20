9b59bf0e5abf92589dd96023e836b1025f33e13a
우병규 <dnqudrb94@naver.com>
2023-04-20 14:50:39 +0900

    회원 파트 머지

--- src/main/java/com/page6/repository/MemberRepository.java
+++ src/main/java/com/page6/repository/MemberRepository.java
@@ -4,7 +4,12 @@ package com.page6.repository;
 import com.page6.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findByEmail(String email);
+
+
+    Optional<Member> findByName(String name);
 }
--- src/main/java/com/page6/service/MemberService.java
+++ src/main/java/com/page6/service/MemberService.java
@@ -3,6 +3,7 @@ package com.page6.service;
 import com.page6.entity.Member;
 import com.page6.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,16 +11,42 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @RequiredArgsConstructor
 @Transactional
 @Service
 //MemberService가 UserDetailsService를 구현
 public class MemberService implements UserDetailsService {
 
-    private final MemberRepository memberRepository;
+    @Autowired
+    private MemberRepository memberRepository;
 
+  @Transactional(readOnly = true)
+  public int fineMember(String email) {
+   Member member = memberRepository.findByEmail(email);
+   if(member == null){
+      return 1;
+     }
+      return 0;
+   }
+
+    @Transactional
+    public void singUp(Member member) {
+
+            memberRepository.save(member);
+
+    }
+
+
+
+
+
+
+
+    @Transactional
     public Member saveMember(Member member) {
         validateDuplicateMember(member);
+
         return memberRepository.save(member);
     }
 
@@ -41,7 +68,7 @@ public class MemberService implements UserDetailsService {
             throw new UsernameNotFoundException(email);
         }
 
-        /* userDetail을 구현하고 이는 user 객체 반환. User 객체를 생성하기 위해서
+        /* userDetail을 구현하고 있는 user 객체 반환. User 객체를 생성하기 위해서
         생성자로 회원의 이메일,비밀번호,role을 파라미터 넘겨줌
          */
         return User.builder()
@@ -51,4 +78,6 @@ public class MemberService implements UserDetailsService {
                 .build();
 
     }
+
+
 }
